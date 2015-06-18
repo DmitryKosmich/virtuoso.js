02c985d8aef3bab6adbca1142f7e9c92852d6adc
Dmitry_Kosmich <kosmich27@gmail.com>
2015-06-18 14:08:21 +0300

    Project structure: added documentation generator

--- gulpfile.js
+++ gulpfile.js
@@ -7,17 +7,18 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 var jasmine = require('gulp-jasmine');
+var shell = require('gulp-shell');
 
 // Lint Task
 gulp.task('lint', function() {
-    return gulp.src('src/js/*.js')
+    return gulp.src('src/js/**/*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
 
 // Concatenate & Minify JS
 gulp.task('scripts', function() {
-    return gulp.src('src/js/*.js')
+    return gulp.src('src/js/**/*.js')
         .pipe(concat('all.js'))
         .pipe(gulp.dest('dist'))
         .pipe(rename('all.min.js'))
@@ -25,9 +26,14 @@ gulp.task('scripts', function() {
         .pipe(gulp.dest('dist'));
 });
 
+// JSDoc documentation
+gulp.task('doc', shell.task([
+    'jsdoc .'
+]));
+
 // Watch Files For Changes
 gulp.task('watch', function() {
-    gulp.watch('src/js/*.js', ['lint', 'scripts']);
+    gulp.watch('src/js/**/*.js', ['lint', 'scripts', 'doc']);
 });
 
 // Test task
